938ac368e41d0247617a92035a1ef4c8a7d1d97a
loay ashraf <117324979+loay160@users.noreply.github.com>
2024-01-26 21:06:29 +0200

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A script that:
 - takes in a URL,
-- sends a request to the URL and displays the value
+- sends a requests to the URL and displays the value
 - of the X-Request-Id variable found in the header ofthe response.
 """
 import sys
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+        print(dict(response.headers).get("X-Requests-Id"))
